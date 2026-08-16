7f2fc61aa3441cf75db1c51255c6af7bea4436a7
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-17 00:19:35 +0530

    Add factorial program

--- Day1/factorial.java
+++ Day1/factorial.java
@@ -3,7 +3,7 @@ package Day1;
 import java.util.Scanner;
 
 public class factorial {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
