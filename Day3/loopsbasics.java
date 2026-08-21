ec74b5b5ddd0de081a482491ca1b899c5321d14e
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-21 19:03:57 +0530

    add loops basics program

---
+++ Day3/loopsbasics.java
@@ -0,0 +1,18 @@
+package Day3;
+import java.util.Scanner;
+public class loopsbasics {
+    public static void main(String[] args) {
+        
+    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter n : ");
+    int n = sc.nextInt();
+    for(int  i=1;i<=n;i++){
+        System.out.println("Hello World");
+    }
+    sc.close();
+    
+
+
+}
+}
