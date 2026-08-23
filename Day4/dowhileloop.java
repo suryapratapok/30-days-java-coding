ee7ef76f2130bf4268e990261782450adf69e89f
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-23 12:21:16 +0530

    add dowhileloop program

---
+++ Day4/dowhileloop.java
@@ -0,0 +1,12 @@
+package Day4;
+
+public class dowhileloop {
+    public static void main(String[] args) {
+        int i = 17;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=8);
+        
+    }
+}
