e3c36867840d4e9f6fd653de865a120c9a02f13c
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-23 12:08:47 +0530

    add whileloop program

---
+++ Day4/whileloop.java
@@ -0,0 +1,11 @@
+package Day4;
+
+public class whileloop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=8) {
+            System.out.println(i+" ");
+            i++;
+        }
+    }
+}
