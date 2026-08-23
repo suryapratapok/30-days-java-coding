46d71bf191fae5a5b93a1485c440e910ea0d029c
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-23 10:24:36 +0530

    add hf program

---
+++ Day3/highestfactor.java
@@ -0,0 +1,18 @@
+package Day3;
+import java.util.Scanner;
+
+public class highestfactor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter n:");
+        int n = sc.nextInt();
+        int hf = 1;
+        for(int i = n-1;i>=1;i--){
+            if(n%i==0){
+                hf = i;
+                break;
+            }
+        }
+        System.out.println(hf);
+    }
+}
