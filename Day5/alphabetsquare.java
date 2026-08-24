8f907bff99ba43485a08d52c04992f37e9dfffc9
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-24 19:07:48 +0530

    add alphabet program

---
+++ Day5/alphabetsquare.java
@@ -0,0 +1,15 @@
+package Day5;
+import java.util.Scanner;
+public class alphabetsquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter n:");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
