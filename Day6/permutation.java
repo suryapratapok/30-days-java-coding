358b5cea00ec4dbd687e6e74a5576213ff7d37a5
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-25 17:15:22 +0530

    add combination program

--- Day6/permutation.java
+++ Day6/permutation.java
@@ -1,24 +1,23 @@
+
 package Day6;
 import java.util.Scanner;
-public class Combination {
-    public static int Fact(int x){
-        int xFact = 1;
-        for(int i=1;i<=x;i++){
-            xFact *= i;
-        }
-        return xFact;
-    }
+public class permutation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter n:");
         int n = sc.nextInt();
         System.out.println("enter r:");
         int r = sc.nextInt();
+        int nfact = 1;
+        for(int i=1;i<=n;i++){
+            nfact *= i;
+        }
+        int rfact = 1;
+        for(int i=1;i<=r;i++){
+            rfact *= i;
+        }
         
-        int nfact = Fact(n);
-        int rfact = Fact(r);
-        int n_rfact = Fact(n-r);
-        int ncr = nfact/(rfact*n_rfact);
+        int ncr = nfact/rfact;
 System.out.println(ncr);
     }
 }
