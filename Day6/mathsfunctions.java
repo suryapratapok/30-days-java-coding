79f19df9db062a4daa7960ce39a956c55ef86ee0
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-25 12:22:01 +0530

    add maths program

---
+++ Day6/mathsfunctions.java
@@ -0,0 +1,16 @@
+package Day6;
+
+public class mathsfunctions {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(5,8));
+        System.out.println(Math.sqrt(3));
+        System.out.println(Math.cbrt(8));
+        System.out.println(Math.abs(-8));
+        System.out.println(Math.floor(-3.14));
+        System.out.println(Math.max(6,9));
+        System.out.println(Math.min(9.8,9.1));
+        int a = 3, b=4, c=9;
+        System.out.println(Math.max(a,Math.max(b,c)));
+    }
+    
+}
