821aaa75320cb053f8a6949af48da3c552147bde
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-25 14:12:58 +0530

    add argument program

---
+++ Day6/passingarguments.java
@@ -0,0 +1,10 @@
+package Day6;
+
+public class passingarguments {
+    public static void main(String[] args) {
+        surya("Modi ji",80);
+    }
+    public static void surya(String name ,int age ) {
+        System.out.println("Hello "+name+ " your age is "+age);
+    }
+}
