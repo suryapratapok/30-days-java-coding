32ac044c258706137af41e08b6f05840e091ae90
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-25 13:07:24 +0530

    add methods program

---
+++ Day6/moreabtmethods.java
@@ -0,0 +1,24 @@
+package Day6;
+
+public class moreabtmethods {
+    
+    public static void  surya() {
+        System.out.println("Surya is the Best");
+    }
+    public static void sp() {
+        System.out.println("Surya achieve anything!");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("My name is Surya Pratap Singh");
+        surya();
+        sp();
+        spsc();
+    }
+public static void spsc() {
+    for(int i=1;i<=6;i++){
+        System.out.println(i+" ");
+    }
+}
+
+}
