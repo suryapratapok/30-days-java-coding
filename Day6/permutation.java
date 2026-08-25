d66fb43aebdbccf1697fe89bc75795f08b1275ea
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-25 17:20:46 +0530

    add permutation program

--- Day6/permutation.java
+++ Day6/permutation.java
@@ -2,20 +2,22 @@
 package Day6;
 import java.util.Scanner;
 public class permutation {
+    public static int Fact(int x){
+        int xFact = 1;
+        for(int i=1;i<=x;i++){
+            xFact *= i;
+        }
+        return xFact;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter n:");
         int n = sc.nextInt();
         System.out.println("enter r:");
         int r = sc.nextInt();
-        int nfact = 1;
-        for(int i=1;i<=n;i++){
-            nfact *= i;
-        }
-        int rfact = 1;
-        for(int i=1;i<=r;i++){
-            rfact *= i;
-        }
+
+        int nfact = Fact(n);
+        int rfact = Fact(r);
         
         int ncr = nfact/rfact;
 System.out.println(ncr);
