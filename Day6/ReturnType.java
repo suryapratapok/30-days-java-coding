1f00867d79955d7e322319a52e4441eec2daaaa3
Surya Pratap Singh <suryapratapok@gmail.com>
2026-08-25 15:19:52 +0530

    add return program

---
+++ Day6/ReturnType.java
@@ -0,0 +1,13 @@
+package Day6;
+
+public class ReturnType {
+public static int sum(int a, int b) {
+    System.out.println("hii im surya");
+    return a+b;
+}
+
+    public static void main(String[] args) {
+       
+        System.out.println(sum(5,9));
+    }
+}
